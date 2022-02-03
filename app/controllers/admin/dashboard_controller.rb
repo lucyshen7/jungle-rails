b731f7af1a240e3489a4c91d4ea01fc1fe49a58e
Lucy Shen <lucyshen77@gmail.com>
2022-02-03 05:56:46 +0000

    Add dynamic links for categories in admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: :index
+
   def show
     @products_count = Product.count(:id)
     @categories_count = Category.count(:id)
+    @categories = Category.all
   end
 end
